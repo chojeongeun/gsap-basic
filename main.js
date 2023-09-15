c550661106d6c0c6d2be849de1fe5e814ec29f5a
chojeongeun <juy0816@naver.com>
2023-09-15 13:18:40 +0900

    기본 scrub 지정

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-let split = new SplitText('h1'); //h1 글자 한개씩 분리
-console.log(split);
-
 let tl = gsap.timeline({
 	scrollTrigger: {
 		trigger: '#sec2',
 		start: 'top 50%',
 		end: 'top 0%',
 		markers: true,
-		toggleActions: 'restart pause reverse reset',
+		// toggleActions: 'restart pause reverse reset',
+		scrub: 1, //스크롤값과 연동되게 해줌 1:살짝 밀리는 효과 (숫자 클수록)
 	},
 });
 
-tl.from(split.chars, { scale: 2, opacity: 0, stagger: 0.1 });
-//내려보면 동기화 안되어있어서 이미 동작 끝나있음
-tl.from('button', { x: 500, opacity: 0 });
+tl.to('.box', {
+	scale: 3,
+	opacity: 0,
+	rotation: 360,
+});
